d38733007a9a3ca3cdb42070306c2db01548dda6
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-06-15 15:48:20 +0900

    create new noborutaro project

---
+++ src/main/java/com/example/NoborutaroApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoborutaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoborutaroApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/domain/Car.java
@@ -0,0 +1,23 @@
+package com.example.domain;
+
+public class Car {
+
+    private String name;
+    private String bodyColor;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getBodyColor() {
+        return bodyColor;
+    }
+    public void setBodyColor(String bodyColor) {
+        this.bodyColor = bodyColor;
+    }
+
+
+
+}
